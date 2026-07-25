0ed3435e46e9657de1e460adfbaf1055d630f133
oktntko <oktntko@gmail.com>
2026-07-26 00:08:03 +0900

    setup ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import { invoke } from '@tauri-apps/api/core';
+import { useState } from 'react';
+
+import './App.css';
+import reactLogo from './assets/react.svg';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }));
   }
 
   return (
