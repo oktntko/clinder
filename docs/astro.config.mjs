1fe55b8c5b40612610b101265f22442d35a9b319
oktntko <oktntko@gmail.com>
2026-09-04 23:24:42 +0900

    workflows

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://oktntko.github.io',
+  base: '/clinder',
   integrations: [
     starlight({
       title: 'Clinder',
