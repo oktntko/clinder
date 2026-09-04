0005a5827bff2db6aa30c95d4b611db6e27fe913
oktntko <oktntko@gmail.com>
2026-09-04 23:22:01 +0900

    docs: base

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,19 +7,16 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
-      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+      title: 'Clinder',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/oktntko/clinder' }],
       sidebar: [
         {
           label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', slug: 'guides/example' },
-          ],
+          items: [{ label: 'Getting Started', slug: 'guides/getting-started' }],
         },
         {
-          label: 'Reference',
-          items: [{ autogenerate: { directory: 'reference' } }],
+          label: 'How to use',
+          items: [{ autogenerate: { directory: 'how-to-use' } }],
         },
       ],
     }),
